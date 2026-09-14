ef5919fc5767cb75ce82bd337c780f1fa8050d20
riteshrana12-dev <ranaritesh677@gmail.com>
2026-09-15 02:33:39 +0530

    feat(node):add the re evaluate node at end to check the ats of tailor resume

--- ai_engine/agents/graph.py
+++ ai_engine/agents/graph.py
@@ -1,6 +1,6 @@
 from langgraph.graph  import StateGraph, START, END
 from ai_engine.agents.state import AgentState
-from ai_engine.agents.nodes.evaluator import evaluate_ats_node
+from ai_engine.agents.nodes.evaluator import evaluate_ats_node, re_evaluate_tailored_ats_node
 from ai_engine.agents.nodes.tailor_resume import tailor_resume_node
 from ai_engine.agents.nodes.rewriter import rewriter_bullets_node
 from ai_engine.agents.nodes.generator import generate_docs_node
@@ -13,13 +13,15 @@ agent_workflow.add_node("evaluate_ats", evaluate_ats_node)
 agent_workflow.add_node("tailor_resume", tailor_resume_node)
 agent_workflow.add_node("rewriter_bullets", rewriter_bullets_node)
 agent_workflow.add_node("generator_docs", generate_docs_node)
+agent_workflow.add_node("re_evaluate_ats", re_evaluate_tailored_ats_node)
 
 # Flow -> Connecting edges between nodes
 agent_workflow.add_edge(START, "evaluate_ats")
 agent_workflow.add_edge("evaluate_ats", "tailor_resume")
 agent_workflow.add_edge("tailor_resume", "rewriter_bullets")
 agent_workflow.add_edge("rewriter_bullets", "generator_docs")
-agent_workflow.add_edge("generator_docs", END)
+agent_workflow.add_edge("generator_docs", "re_evaluate_ats")
+agent_workflow.add_edge("re_evaluate_ats", END)
 
 compiled_workflow = agent_workflow.compile()
 
