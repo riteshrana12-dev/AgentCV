ed4c7d88db5502b4d4271ea402e062d580a70322
riteshrana12-dev <ranaritesh677@gmail.com>
2026-09-17 01:26:16 +0530

    chore(server): http server instance from websocket

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,8 @@
-import app from "./index";
+// import app from "./index";
+import { server } from "./websocket/server";
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
