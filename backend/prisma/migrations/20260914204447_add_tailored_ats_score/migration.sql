b3bf03bd211714aed7814e9690e6a98107f8bbf8
riteshrana12-dev <ranaritesh677@gmail.com>
2026-09-16 21:40:05 +0530

    feat(migration): sql migration

---
+++ backend/prisma/migrations/20260914204447_add_tailored_ats_score/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "Resume" ALTER COLUMN "tailoreResumeUrlDocx" DROP NOT NULL,
+ALTER COLUMN "tailoredResumeUrlPdf" DROP NOT NULL,
+ALTER COLUMN "tailoredAtsScore" DROP NOT NULL;
