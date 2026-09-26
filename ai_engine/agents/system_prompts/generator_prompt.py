83b38c426580cbb0fc55e49f859333693afb0837
riteshrana12-dev <ranaritesh677@gmail.com>
2026-09-27 01:37:06 +0530

    chore(prompt):modification in prompt in outreach part

--- ai_engine/agents/system_prompts/generator_prompt.py
+++ ai_engine/agents/system_prompts/generator_prompt.py
@@ -6,9 +6,11 @@ Missing Skills: {missing_skills}
 Job Description: {raw_jd}
 Resume Context: {raw_resume}
 
+This below output the cover_letter , cold_email and linkedin_message this three it should be in professional outreach way .
+
 Return ONLY a JSON object:
 {{
-  "cover_letter": "Dear Hiring Manager,...",
+  "cover_letter": "To, Hiring Manager,...",
   "cold_email": "Subject: ...\\n\\nHi [Hiring Manager],...",
   "linkedin_message": "Hi [Name], I noticed your team..."
 }}
